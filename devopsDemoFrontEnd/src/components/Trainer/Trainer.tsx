dc99bfbf2b864de5ef739cc4d5d88275f27e5742
Kung Lo <kungyang.lo@revature.com>
2024-08-20 06:47:07 -0400

    More trainers

--- devopsDemoFrontEnd/src/components/Trainer/Trainer.tsx
+++ devopsDemoFrontEnd/src/components/Trainer/Trainer.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface Trainer {
+    trainerId: number,
+    username: string,
+    password: string
+}
 
 function Trainer() {
+    const [trainerList, setTrainerList] = useState<Trainer[] | null> (null);
+
+    const url = 'https://aspdemo22-f5dsewgrgma5aggs.eastus2-01.azurewebsites.net/api/trainer';
+
+    useEffect(() => {
+        async function fetchTrainer() {
+            try {
+                const res = await axios.get(url);
+                setTrainerList(res.data);
+            } catch (error) {
+                console.error('Error: ', error);
+            }
+        }
+
+        fetchTrainer();
+    }, []);
+
     return (
     <>
-        <h1>Trainer</h1>
+    <div style={{margin: 0}}>
+        <header>
+            <h1 style={{textAlign:"center"}}>Trainer</h1>
+        </header>
+        <article>
+            {trainerList ? <ul style={{listStyleType:"none", position:"absolute", left:"50%", width:"30%", transform: "translate(-50%)"}}>
+                {trainerList.map(t =>(
+                    <li key={t.trainerId} style={{border:"solid", margin:"10px"}}>
+                        <h2>Name: {t.username}</h2>
+                        <h2>Password: {t.password}</h2>
+                    </li>
+                ))}
+            </ul> : <p>There are no pokemon</p>}
+        </article>
+    </div>
     </>
     );
 }
